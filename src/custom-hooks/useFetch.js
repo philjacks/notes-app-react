8d267baef8aab160eff292b4350f0cafc3792003
Phil <philjacks.1987@gmail.com>
2021-09-22 23:13:19 +0100

    'edits'

--- src/custom-hooks/useFetch.js
+++ src/custom-hooks/useFetch.js
@@ -9,29 +9,29 @@ const useFetch = (url) => {
   useEffect(() => {
     const abortCont = new AbortController();
 
-    setTimeout(() => {
-      fetch(url, { signal: abortCont.signal })
-        .then(res => {
-          if (!res.ok) {
-            throw Error("Could not fetch data from resource");
-          }
-          return res
-            .json()
-            .then(data => {
-              setData(data);
+
+    fetch(url, { signal: abortCont.signal })
+      .then(res => {
+        if (!res.ok) {
+          throw Error("Could not fetch data from resource");
+        }
+        return res
+          .json()
+          .then(data => {
+            setData(data);
+            setIsLoading(false);
+            setError(null);
+          })
+          .catch(err => {
+            if (err.name === 'AbortError') {
+              console.log("Fetch Aborted");
+            } else {
               setIsLoading(false);
-              setError(null);
-            })
-            .catch(err => {
-              if (err.name === 'AbortError') {
-                console.log("Fetch Aborted");
-              } else {
-                setIsLoading(false);
-                setError(err.message);
-              }
-            });
-        });
-    }, 1000)
+              setError(err.message);
+            }
+          });
+      });
+
 
     return () => abortCont.abort()
   }, [url]);
