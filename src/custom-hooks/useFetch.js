70d04d2649a6b320bcd3fdb408cd47cb2807b435
philjacks <77470834+philjacks@users.noreply.github.com>
2021-09-25 20:28:30 +0100

    error bugs fixed

--- src/custom-hooks/useFetch.js
+++ src/custom-hooks/useFetch.js
@@ -5,35 +5,32 @@ const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-
   useEffect(() => {
     const abortCont = new AbortController();
 
-
     fetch(url, { signal: abortCont.signal })
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
           throw Error("Could not fetch data from resource");
         }
-        return res
-          .json()
-          .then(data => {
-            setData(data);
-            setIsLoading(false);
-            setError(null);
-          })
-          .catch(err => {
-            if (err.name === 'AbortError') {
-              console.log("Fetch Aborted");
-            } else {
-              setIsLoading(false);
-              setError(err.message);
-            }
-          });
+        return res.json();
+      })
+
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("Fetch Aborted");
+        } else {
+          setIsLoading(false);
+          setError(err.message);
+        }
       });
 
-
-    return () => abortCont.abort()
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isLoading, error };
